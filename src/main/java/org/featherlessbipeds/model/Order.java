f2c9cf6ad049bef9fe943aa259451c182e501cdd
joaoTorpe <victotorpe10@gmail.com>
2025-03-03 17:17:24 -0300

    Order entity, repository and service layers

--- src/main/java/org/featherlessbipeds/model/Order.java
+++ src/main/java/org/featherlessbipeds/model/Order.java
@@ -1,14 +1,22 @@
 package org.featherlessbipeds.model;
 
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 import org.featherlessbipeds.Enums.ServicesCategoriesEnum;
 
 @Builder
+@Getter
+@Setter
 public class Order {
-
+    private Integer id;
     private ServicesCategoriesEnum serviceCategory;
     private Integer termInDays;
     private String description;
     private Double budget;
     private String attachment;
+    private LocationEntity location;
+    private Client client;
+    private Freelancer freelancer;
+
 }
