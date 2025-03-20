390400e9f69fda44771a221b935bbe67ec73bfbf
JoaoVitorGl <vitorgouveia06@gmail.com>
2025-03-20 10:18:06 -0300

    feat: Client entity, repository and service

---
+++ src/main/java/org/featherlessbipeds/gouveia/repository/ClientRepository.java
@@ -0,0 +1,17 @@
+package org.featherlessbipeds.gouveia.repository;
+
+import org.featherlessbipeds.gouveia.entity.Client;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientRepository
+{
+    private List<Client> clients = new ArrayList<>();
+    public boolean existsByEmail(String email) {
+        return clients.stream().anyMatch(client -> client.getEmail().equals(email));
+    }
+    public void save(Client client) {
+        clients.add(client);
+    }
+}
